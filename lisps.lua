8d971fe0eb91a8dd72ea6df68fa30a3f6443ef8e
Jonas <jonaslindstrom0000@gmail.com>
2023-11-12 15:26:21 +0000

    added flag to disable column symbols

--- lisps.lua
+++ lisps.lua
@@ -1,2 +1,10 @@
+vim.diagnostic.config({
+    virtual_text = true,  -- Enable/disable virtual text
+    signs = false,        -- Disable signs in the sign column
+    underline = true,     -- Enable/disable underline
+    update_in_insert = false,
+    severity_sort = true,
+})
+
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.tsserver.setup{}
