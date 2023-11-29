9901c9c37fb0b1c31816f1422bec0d837b9a0324
Jonas <jonaslindstrom0000@gmail.com>
2023-11-29 15:01:15 +0000

    added keybind for opening menu

--- tele.lua
+++ tele.lua
@@ -1,11 +1,11 @@
-require("telescope").load_extension "file_browser"
+require("telescope").load_extension("file_browser")
 
-require("telescope").setup {
-  extensions = {
-    file_browser = {
-      theme = "catppuccin-mocha",
-    },
-  },
-}
+require("telescope").setup({
+	extensions = {
+		file_browser = {
+			theme = "catppuccin-mocha",
+		},
+	},
+})
 
-
+vim.api.nvim_set_keymap("n", "<C-F>", ":Telescope file_browser<CR>", { noremap = true, silent = true })
